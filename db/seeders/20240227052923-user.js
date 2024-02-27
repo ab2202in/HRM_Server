69c0f39ffb3ecd3fb2dd25c526aa43cc5e1ae3a6
abin sasikumar <abinsasikumar22@gmail.com>
2024-02-27 11:41:37 +0530

    server seeding

---
+++ db/seeders/20240227052923-user.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: (models, mongoose) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return models.Test.bulkWrite([
+        {
+          insertOne: {
+            document: {
+              name: 'first test'
+            }
+          }
+        }
+      ]).then(res => {
+      // Prints "1"
+      console.log(res.insertedCount);
+    });
+    */
+    return models.users
+    .insertMany([
+      {
+        _id: "65dd792357bafc18dba412f5",
+        name: "aadhi",
+        email : "aadhi007@gmail.com"
+        
+      }
+    ])
+  },
+
+  down: (models, mongoose) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:*/
+      return models.users([
+        {
+          deleteOne: {
+             _id: {
+              $in :[
+                "65dd792357bafc18dba412f5"
+              ]
+            
+            }
+          }
+        }
+      ]).then(res => {
+      // Prints "1"
+      console.log(res.deletedCount);
+      });
+    
+  }
+};
